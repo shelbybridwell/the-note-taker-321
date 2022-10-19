27d601d68f8b73ac98adee34136b1a556e3d1ca1
Shelby Bridwell <109699848+shelbybridwell@users.noreply.github.com>
2022-10-18 21:31:39 -0500

    express

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use();
+app.use((req, res, next) => {
+  console.log('Time:', Date.now())
+  next()
+})
 
 let noteTitle;
 let noteText;
